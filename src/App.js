0e1c7cb103c770b7bd1322fd750ddebe5f7fbcb4
Lucas Dreher <lucasdreher@gmail.com>
2022-02-03 08:49:44 +0100

    Redux & Async Code Using useEffect with Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
-import { useSelector } from 'react-redux';
-
 function App() {
 	const showCart = useSelector((state) => state.ui.cartIsVisible);
+	const cart = useSelector((state) => state.cart);
+
+	useEffect(
+		() => {
+			fetch('https://react-http-88f71-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+				method: 'PUT',
+				body: JSON.stringify(cart)
+			});
+		},
+		[ cart ]
+	);
 
 	return (
 		<Layout>
